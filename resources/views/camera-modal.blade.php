9699be0d52c29d42cdb39359569e765cef57f02b
Bruce Lampson <brucelampson@gmail.com>
2020-01-22 23:53:37 -0600

    Updates modal

--- resources/views/camera-modal.blade.php
+++ resources/views/camera-modal.blade.php
@@ -1,9 +1,11 @@
 <div id="profile-photo-camera-modal" class="modal fade" tabindex="-1">
-    <div class="modal-dialog">
+    <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <span class="close" data-dismiss="modal">&times;</span>
-                <h4 class="modal-title">Take a picture</h4>
+                <h5 class="modal-title">Modal title</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
             </div>
             <div class="modal-body">
                 <div class="alert alert-danger" v-if="form.errors.has('photo')">
@@ -13,8 +15,8 @@
                 <div id="profile-photo-camera-preview"></div>
             </div>
             <div class="modal-footer">
-                <button type="button" class="btn btn-default pull-left" :disabled="form.busy" data-dismiss="modal">Cancel</button>
-                <button type="button" class="btn btn-primary" :disabled="form.busy" @click="takePicture">Take picture</button>
+                <button type="button" class="btn btn-default float-left" :disabled="form.busy" data-dismiss="modal">{{__('Cancel')}}</button>
+                <button type="button" class="btn btn-primary" :disabled="form.busy" @click="takePicture">{{__('Take Picture')}}</button>
             </div>
         </div>
     </div>
